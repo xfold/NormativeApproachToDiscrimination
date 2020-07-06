d4ccbe30ddd13246f411c6f0186e67dc59a65d2b
xfold <xfaran@gmail.com>
2020-07-06 11:35:04 +0100

    updated run.py file

--- Run.py
+++ Run.py
@@ -24,4 +24,5 @@ print('''
 The dataset was collected from https://archive.ics.uci.edu/ml/datasets/statlog+(german+credit+data) and discretised using quantile discretisation.
 ''')
 na = NormativeApproachDiscrimination('DatasetsClean/german_credit_quantile/german_credit_quantile.csv', 'DatasetsClean/german_credit_quantile/config_german_credit_quantile.py', verbose= False)
-
+violations = na.Run()
+pprint.pprint(violations)
